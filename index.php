49222547e1582ca97d12dab35c73cd37a91b4bf0
jfscalante <scalante2002@gmail.com>
2023-11-25 11:46:52 -0500

    adelanto imagen propia y enlace con git y linkidIn

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <title>portafolio Fabian Scalante</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
   </head>
   <body>
     <!--barra de navegacion-->
@@ -38,18 +39,27 @@
 <!--termina barra de navegacion-->
 
 
-<div class="container text-center">
+<div class="container text-center ">
         <div class="row row-cols-2">
-          <div class="col-md-6">
-            <img   class = "img-fluid" class="img-thumbnail" src="scalante.jfif.jpg" alt="" class="">
+          <div class="col-md-6 d-grid gap-2 col-6 mx-auto col-10">
+            <img src="img1.png" class="imagen img-fluid rounded float-end" alt="...">
           </div>
-          <div class="col-md-6">
-            <p>Hello, i´m</p>
-            <h1>Fabian Scalante</h1>
-            <h2>Analista y Desarrollador de Software</h2>
-          </div>
+                <div class="col-md-6 pt-5 textostyle" style="min-height: 100vh;" style="padding:  30%;">
+                        <p>Hello, I'm</p>
+                        <h1>Fabian scalante</h1>
+                        <h3>Software Analyst and Developer</h3>
+                      
+                        <button type="button" class="btn btn-outline-dark btn-sm">Download CV</button>
+                    
+                        <button type="button" class="btn btn-dark btn-sm">Contact Info</button> <br><br>
+                        <a href="https://github.com/jfscalante"><i class="fa-brands fa-github fa-2xl" style="color: #000000;"></i></a>
+                        <a href="https://www.linkedin.com/in/fabian-scalante-claros-0ab6122a1/"><i class="fa-brands fa-linkedin fa-2xl" style="color: #000000;"></i></a>
+                </div>
         </div>
       </div>
+
+
+       
 
 
 
